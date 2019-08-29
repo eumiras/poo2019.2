f5769dcfa71281be157d4226f884519ba9fcf4af
eumiras <51832486+eumiras@users.noreply.github.com>
2019-08-29 12:00:10 -0300

    Add files via upload

---
+++ help.java
@@ -0,0 +1,64 @@
+import java.util.Scanner
+
+
+class Unicornio{
+	int nPass;
+	int nPassMax;
+	int carinhometro;
+	int carinhoMax;
+	int quilometragem;
+	boolean estarvivo = true;
+	
+	
+	
+	void voar(int distanciaEmKm) {
+		if (nPass < 1) {
+			System.out.println("não gosto de voar sozinho! >.<");
+			return;
+			if (nPass > 3) {
+				System.out.println)("pessoas demais aaaaa");
+				return
+			}
+		}
+		if (carinhometro < 1) {
+			System.out.println("me dê carinho humano!")
+			return;
+			
+		}
+	
+		
+	/**subir(int qtd)
+		 nao sobe alem do max
+		 
+	darAmor(qtd)
+		aumenta carinhometro
+		
+	brigar(qtd)
+		diminui carinhometro**/
+		
+		
+
+	
+}
+
+
+
+package sem2au1;
+
+public class help {
+
+	public static void main(String[] args) {
+	Scanner scanner = new Scanner(System.in);
+	
+	
+	Unicornio mimi = new Unicornio();
+	String line;
+	while(mimi.estarvivo) {
+		line = scanner.nextLine();
+		String
+	}
+		
+
+	}
+
+}
